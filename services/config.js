76af162df9da1b80480be01090623c28f73eedb1
KamalDas492 <104711607+KamalDas492@users.noreply.github.com>
2024-06-21 17:34:22 +0530

    Update config.js

--- services/config.js
+++ services/config.js
@@ -5,13 +5,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyuFdh04q7eqoqnt3XwreU9RmTk6VdNH8",
-  authDomain: "quizworld-app-727d5.firebaseapp.com",
-  projectId: "quizworld-app-727d5",
-  storageBucket: "quizworld-app-727d5.appspot.com",
-  messagingSenderId: "129759240254",
-  appId: "1:129759240254:web:de908d0423b8d9c40b716f",
-  measurementId: "G-3C5CH24B7X"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
